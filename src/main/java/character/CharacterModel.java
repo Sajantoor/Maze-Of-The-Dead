a4fc1d35a358bb592654893118703dd4c456d0c6
Kaung722 <70074279+Kaung722@users.noreply.github.com>
2021-11-03 02:45:32 -0700

    feat: Implement CharacterModel and Player
    - Wrote getters and setters for CharacterModel and Player classes
    - Wrote move methods for CharacterModel class
    - Wrote Javadocs

--- src/main/java/character/CharacterModel.java
+++ src/main/java/character/CharacterModel.java
@@ -2,22 +2,107 @@ package main.java.character;
 
 import main.java.utilities.*;
 
+/**
+ * Controls the Position of the Character
+ *
+ * @author Kaung Si Thu
+ * @see Position
+ */
 public class CharacterModel {
     private Position position;
 
+    /**
+     * CharacterModel constructor function
+     *
+     * @param pos Position of the character
+     * @see Position
+     */
+    protected CharacterModel(Position pos) {
+        this.position = pos;
+    }
+
+    /**
+     * Returns the Position of the character
+     *
+     * @return Position of the character
+     * @see Position
+     */
     public Position getPosition() {
         return position;
     }
 
+    /**
+     * Sets the Position argument as the position of the character
+     *
+     * @param pos the Position instance containing x and y coordinates
+     * @see Position
+     */
     public void setPosition(Position pos) {
         this.position = pos;
     }
 
+    /**
+     * Sets the arguments as x and y coordinates of the Position of the character
+     *
+     * @param x x coordinate of a position
+     * @param y y coordinate of a position
+     * @see Position
+     */
     public void setPosition(int x, int y) {
-        // TODO: Implement me!
+        this.position.setX(x);
+        this.position.setY(y);
     }
 
-    public void move(Movement direction) {
-        // TODO: Implement me!
+    private void move(Movement direction) {
+        switch (direction) {
+            case FORWARD:
+                position.setY(position.getY() - 1);
+                break;
+            case BACKWARD:
+                position.setY(position.getY() + 1);
+                break;
+            case LEFT:
+                position.setX(position.getX() - 1);
+                break;
+            case RIGHT:
+                position.setX(position.getX() + 1);
+                break;
+        }
+    }
+
+    /**
+     * Moves the character one unit up, changing the Position
+     *
+     * @see Position
+     */
+    public void moveForward() {
+        this.move(Movement.FORWARD);
+    }
+
+    /**
+     * Moves the character one unit down, changing the Position
+     *
+     * @see Position
+     */
+    public void moveBackward() {
+        this.move(Movement.BACKWARD);
+    }
+
+    /**
+     * Moves the character to the left, changing the Position
+     *
+     * @see Position
+     */
+    public void moveLeft() {
+        this.move(Movement.LEFT);
+    }
+
+    /**
+     * Moves the character to the right, changing the Position
+     *
+     * @see Position
+     */
+    public void moveRight() {
+        this.move(Movement.RIGHT);
     }
 }
