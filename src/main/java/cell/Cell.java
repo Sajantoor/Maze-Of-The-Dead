785f6b4057973a8d694d21c30b178549f2af8081
dylany <dylan_young@sfu.ca>
2021-11-08 22:18:37 -0800

    feat: Added constructors, methods and javadoc comments

--- src/main/java/cell/Cell.java
+++ src/main/java/cell/Cell.java
@@ -2,32 +2,73 @@ package main.java.cell;
 
 import main.java.utilities.Position;
 
+/**
+ * Represents a cell at a position in a maze that can contain either a path, wall, start point, end point, reward, or trap.
+ *
+ * @author Dylan Young
+ */
+
 public class Cell {
     private Position position;
-    private CellType cellType;
+    private CellType cellType = CellType.WALL;
 
+    /**
+     * Represents a cell in a maze.
+     *
+     * @param position the position where the cell is positioned.
+     * @param cellType defines what the cell is
+     */
     public Cell(Position position, CellType cellType) {
         this.position = position;
         this.cellType = cellType;
     }
 
+    /**
+     * Represents a cell in a maze.
+     *
+     * @param position the position where the cell is positioned
+     */
+    public Cell(Position position) {
+        this.position = position;
+    }
+
+    /**
+     *
+     * @return the current cell
+     */
     public CellType getCellType() {
         return cellType;
     }
 
+    /**
+     * Changes the content of the cell
+     * @param cellType
+     */
     public void setCellType(CellType cellType) {
         this.cellType = cellType;
     }
 
+    /**
+     * Returns the position of the cell
+     * @return
+     */
     public Position getPosition() {
         return position;
     }
 
-    public void setPosition(Position position) {
-        this.position = position;
+    /**
+     * Returns the x position of the cell
+     * @return the x position of the cell
+     */
+    public int getX() {
+        return position.getX();
     }
 
-    public void setPosition(int x, int y) {
-        position = new Position(x,y);
+    /**
+     * Returns the y position of the cell
+     * @return the y position of the cell
+     */
+    public int getY() {
+        return position.getY();
     }
 }
--- src/main/java/game/Maze.java
+++ src/main/java/game/Maze.java
@@ -1,42 +1,179 @@
 package main.java.game;
 
 import main.java.cell.Cell;
+import main.java.cell.CellType;
 import main.java.utilities.Position;
 
+import java.util.ArrayList;
+
+/**
+ * Represents a maze which is comprised of Cells arranged in a 2D grid.
+ *
+ * @author Dylan Young
+ */
 public class Maze {
     private Cell[][] maze;
 
-    public Maze(int height, int width) {
-        // TODO: Implement me!
+    /**
+     * Represents a randomly generated maze that the game will be played on.
+     * This maze will be composed of walls, paths, a start point and an end point.
+     *
+     * @param width  the absolute width of the maze
+     * @param height the absolute height of the maze
+     */
+    public Maze(int width, int height, int numRooms) {
+        maze = new Cell[width][height];
+        newMaze(width, height, numRooms);
     }
 
+    /**
+     * Returns a cell at position (x,y)
+     *
+     * @param x the x position of the cell in the maze
+     * @param y the y position of the cell in the maze
+     * @return the cell of the maze the position (x,y)
+     * @see Cell
+     */
     public Cell getCell(int x, int y) {
-        // TODO: Implement me!
-        return null;
+        return maze[x][y];
     }
 
-    public Cell getCell(Position position) {
-        // TODO: Implement me!
-        return null;
-    }
-
+    /**
+     * Sets the cell type of cell at position (x, y) to a start point
+     *
+     * @param x the x position of the cell in the maze
+     * @param y the y position of the cell in the maze
+     * @see Cell
+     * @see CellType
+     */
     public void setStart(int x, int y) {
-        // TODO: Implement me!
+        maze[x][y].setCellType(CellType.START);
     }
 
-    public void setStart(Position position) {
-        // TODO: Implement me!
-    }
-
+    /**
+     * Sets the cell type of cell at position (x, y) to an end point
+     *
+     * @param x the x position of the cell in the maze
+     * @param y the y position of the cell in the maze
+     * @see Cell
+     * @see CellType
+     */
     public void setEnd(int x, int y) {
-        // TODO: Implement me!
+        maze[x][y].setCellType(CellType.END);
     }
 
-    public void setEnd(Position position) {
-        // TODO: Implement me!
+    /**
+     * Creates a new maze with walls and start point at the top left corner and end point at bottom right corner
+     *
+     * @param width  the absolute width of the maze
+     * @param height the absolute height of the maze
+     */
+    public void newMaze(int width, int height, int numRooms) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                maze[j][i] = new Cell(new Position(j, i));
+            }
+        }
+
+        generateMaze(height, width);
+
+        addRooms(numRooms, width, height);
+
+        setStart(1, 1);
+        setEnd(width - 1, height - 1);
+    }
+
+    /**
+     * Creates rooms at random positions, with random heights and widths.
+     * Rooms are large areas with no walls to give the player more freedom to move
+     *
+     * @param numRooms the number of rooms that will be generated in the maze
+     */
+    public void addRooms(int numRooms, int mazeWidth, int mazeHeight) {
+        for (int i = 0; i < numRooms; i++) {
+
+            int x = (int) ((Math.random() * (mazeWidth - 2)) + 1);
+            int y = (int) ((Math.random() * (mazeHeight - 2)) + 1);
+            int width = 0;
+            int height = 0;
+
+            do {
+                width = randomDimension();
+            } while (width + x >= mazeWidth);
+
+            do {
+                height = randomDimension();
+            } while (height + y >= mazeHeight);
+
+            addRoom(x, y, width, height);
+        }
+    }
+
+    private void addRoom(int x, int y, int width, int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                maze[x + j][y + i].setCellType(CellType.PATH);
+            }
+        }
+    }
+
+    private int randomDimension() {
+        return (int) (Math.random() * 2 + 1);
     }
 
     private void generateMaze(int height, int width) {
-        // TODO: Implement me!
+        ArrayList<Cell> cells = new ArrayList<>();
+
+        int x = (int) ((Math.random() * (width - 2)) + 1);
+        int y = (int) ((Math.random() * (height - 2)) + 1);
+
+        maze[x][y].setCellType(CellType.PATH);
+
+        cells = addCellsToList(cells, height, width, x, y);
+
+        while (cells.size() > 0) {
+            int i = (int) (Math.random() * cells.size() - 1);
+
+            x = cells.get(i).getX();
+            y = cells.get(i).getY();
+            int count = numberAdjacentPath(x, y);
+
+            if (count == 1) {
+                maze[x][y].setCellType(CellType.PATH);
+
+                cells = addCellsToList(cells, height, width, x, y);
+            }
+            cells.remove(i);
+        }
+
+    }
+
+    private ArrayList<Cell> addCellsToList(ArrayList<Cell> cells, int height, int width, int x, int y) {
+
+        if (x != 1)
+            cells.add(maze[x - 1][y]);
+        if (x != width - 2)
+            cells.add(maze[x + 1][y]);
+        if (y != 1)
+            cells.add(maze[x][y - 1]);
+        if (y != height - 2)
+            cells.add(maze[x][y + 1]);
+
+        return cells;
+    }
+
+    private int numberAdjacentPath(int x, int y) {
+        int count = 0;
+
+        if (maze[x + 1][y].getCellType() == CellType.PATH)
+            count++;
+        if (maze[x - 1][y].getCellType() == CellType.PATH)
+            count++;
+        if (maze[x][y + 1].getCellType() == CellType.PATH)
+            count++;
+        if (maze[x][y - 1].getCellType() == CellType.PATH)
+            count++;
+
+        return count;
     }
 }
