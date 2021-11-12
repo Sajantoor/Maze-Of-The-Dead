797b66c1804d0a37d04a235bfac5a0ea0d9b28c6
dylany <dylan_young@sfu.ca>
2021-11-12 07:28:34 -0800

    feat: Created basic design for UI

--- src/main/java/ui/GameUI.java
+++ src/main/java/ui/GameUI.java
@@ -1,5 +1,24 @@
 package ui;
 
+import javax.swing.*;
+
 public class GameUI {
-    // TODO: Implement me!
+    private JFrame frame;
+    private TitleScreen titleScreen;
+    private InstructionScreen instructionScreen;
+    public GameUI(){
+        titleScreen = new TitleScreen();
+        instructionScreen = new InstructionScreen();
+
+        //Full Screen
+        frame = new JFrame("Maze of The Dead");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+
+        //comment and uncomment to test screens
+        frame = titleScreen.getTitleScreen(frame);
+        //frame = instructionScreen.getInstructionsScreen(frame);
+
+        frame.setVisible(true);
+    }
 }
