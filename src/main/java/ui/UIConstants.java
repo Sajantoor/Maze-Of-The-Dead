91905fe439965812a07f42302bec7beb47e8fc5f
dylany <dylan_young@sfu.ca>
2021-11-13 16:00:08 -0800

    feat: Created Utils for UI

---
+++ src/main/java/ui/UIConstants.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class UIConstants {
+    public static final Font heading = new Font("Arial", Font.BOLD, 50);
+    public static final Font plainArial35 = new Font("Arial", Font.PLAIN, 35);
+    public static final Font boldArial35 = new Font("Arial", Font.BOLD, 35);
+    public static final Font plainArial20 = new Font("Arial", Font.PLAIN, 20);
+    public static final Font boldArial20 = new Font("Arial", Font.BOLD, 20);
+    public static final Border keyBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+}
---
+++ src/main/java/ui/UIUtils.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class UIUtils {
+    public static void buttonLayout(JButton button){
+        button.setForeground(Color.BLACK);
+        button.setFont(UIConstants.boldArial35);
+        button.setFocusPainted(false);
+        button.setMargin(new Insets( 0, 0, 0, 0));
+        button.setBorderPainted(false);
+        button.setOpaque(false);
+        button.setBackground(null);
+    }
+
+    public static void addSpace(JPanel panel,int width, int height) {
+        panel.add(Box.createRigidArea(new Dimension(width, height)));
+    }
+}
