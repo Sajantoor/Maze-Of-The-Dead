3d6cfa8ff112b7e840f4f8556b53cd7e90c6b3e8
dylany <dylan_young@sfu.ca>
2021-11-13 16:03:30 -0800

    fix: Added utils standards to TitleScreen

--- src/main/java/ui/TitleScreen.java
+++ src/main/java/ui/TitleScreen.java
@@ -13,33 +13,31 @@ public class TitleScreen {
     public JFrame getTitleScreen(JFrame frame) {
         titleScreenPanel = new JPanel();
         titleScreenPanel.setLayout(new BoxLayout(titleScreenPanel, BoxLayout.PAGE_AXIS));
-        addSpace(400);
-        titleScreenPanel.setBackground(Color.BLACK);
+        UIUtils.addSpace(titleScreenPanel, 500, 400);
 
         //Name of the game displayed
         titleLabel = new JLabel("Maze of The Dead");
-        titleLabel.setForeground(Color.WHITE);
-        titleLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        titleLabel.setFont(UIConstants.heading);
         titleScreenPanel.add(titleLabel);
 
-        addSpace(30);
+        UIUtils.addSpace(titleScreenPanel, 0, 30);
 
         //Start Button
         startButton = new JButton("Start");
-        buttonLayout(startButton);
+        UIUtils.buttonLayout(startButton);
         titleScreenPanel.add(startButton);
 
-        addSpace(30);
+        UIUtils.addSpace(titleScreenPanel, 0, 30);
 
         //Quit button
         quitButton = new JButton("Quit");
-        buttonLayout(quitButton);
+        UIUtils.buttonLayout(quitButton);
         titleScreenPanel.add(quitButton);
-        addSpace(30);
+        UIUtils.addSpace(titleScreenPanel, 0, 30);
 
         //Instruction button
         instructionButton = new JButton("Instruction");
-        buttonLayout(instructionButton);
+        UIUtils.buttonLayout(instructionButton);
         titleScreenPanel.add(instructionButton);
 
         frame.add(titleScreenPanel);
@@ -48,20 +46,6 @@ public class TitleScreen {
 
     public JPanel getTitleScreen() {
         return titleScreenPanel;
-    }
-
-    private void buttonLayout(JButton button) {
-        button.setForeground(Color.WHITE);
-        button.setFont(new Font("Arial", Font.BOLD, 30));
-        button.setFocusPainted(false);
-        button.setMargin(new Insets(0, 0, 0, 0));
-        button.setBorderPainted(false);
-        button.setOpaque(false);
-        button.setBackground(null);
-    }
-
-    private void addSpace(int height) {
-        titleScreenPanel.add(Box.createRigidArea(new Dimension(500, height)));
     }
 
 }
