835f0a19edef3434def53e7d58745bea618f9292
dylany <dylan_young@sfu.ca>
2021-11-13 23:13:18 -0800

    fix: Changed to static for frame and methods

--- src/main/java/ui/GameUI.java
+++ src/main/java/ui/GameUI.java
@@ -3,22 +3,31 @@ package ui;
 import javax.swing.*;
 
 public class GameUI {
-    private JFrame frame;
-    private TitleScreen titleScreen;
-    private InstructionScreen instructionScreen;
-    public GameUI(){
-        titleScreen = new TitleScreen();
-        instructionScreen = new InstructionScreen();
+    private static JFrame frame;
 
+    public GameUI(){
         //Full Screen
         frame = new JFrame("Maze of The Dead");
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setUndecorated(true);
 
-        //comment and uncomment to test screens
-        frame = titleScreen.getTitleScreen(frame);
-        //frame = instructionScreen.getInstructionsScreen(frame);
+        addTitlesScreen();
 
         frame.setVisible(true);
     }
+    public static JFrame getFrame(){
+        return frame;
+    }
+
+    public static void addInstructionScreen(){
+        InstructionScreen instructionScreen = new InstructionScreen();
+        frame.add(instructionScreen.getInstructionScreen());
+    }
+    public static void addTitlesScreen(){
+        TitleScreen titleScreen = new TitleScreen();
+        frame.add(titleScreen.getTitleScreen());
+    }
+
+    //create static methods to call for your panel below. Follow format above
+
 }
