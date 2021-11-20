8ebf9b82531018d7dfa7e9618881877a22ab2970
dylany <dylan_young@sfu.ca>
2021-11-19 18:58:51 -0800

    feat: Added keyboard listener

---
+++ src/main/java/ui/KeyboardListener.java
@@ -0,0 +1,52 @@
+package ui;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyboardListener extends KeyAdapter {
+    @Override
+    public void keyPressed(KeyEvent event) {
+        switch (event.getKeyCode()) {
+            case KeyEvent.VK_W:
+                System.out.println("w");
+                break;
+            case KeyEvent.VK_A:
+                System.out.println("a");
+                break;
+            case KeyEvent.VK_S:
+                System.out.println("s");
+                break;
+            case KeyEvent.VK_D:
+                System.out.println("d");
+                break;
+            case KeyEvent.VK_ESCAPE:
+                System.out.println("esc");
+                break;
+            default:
+                break;
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent event) {
+        switch (event.getKeyCode()) {
+            case KeyEvent.VK_W:
+                System.out.println("w");
+                break;
+            case KeyEvent.VK_A:
+                System.out.println("a");
+                break;
+            case KeyEvent.VK_S:
+                System.out.println("s");
+                break;
+            case KeyEvent.VK_D:
+                System.out.println("d");
+                break;
+            case KeyEvent.VK_ESCAPE:
+                System.out.println("esc");
+                break;
+            default:
+                break;
+        }
+    }
+
+}
