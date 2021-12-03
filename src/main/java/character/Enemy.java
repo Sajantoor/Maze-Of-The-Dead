eaab071259a2f491ab0f64ec48924233209b0244
Sajan Toor <sajantoor98@gmail.com>
2021-12-02 23:36:47 -0800

    fix: Enemy now gets the shortest path
    - Instead of using depth first serach, switched to breadth first search to get the shortest path
    - This is because depth first search would sometimes make questionable moves and it doesn't look like the enemy is tracking the player.

--- src/main/java/character/Enemy.java
+++ src/main/java/character/Enemy.java
@@ -1,102 +1,86 @@
 package character;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 
-import maze.Cell;
 import maze.Maze;
 import utilities.Position;
 
 public class Enemy extends CharacterModel {
     ArrayList<Position> path;
+    ArrayList<Position> nextPath;
 
     public Enemy(Position pos) {
         super(pos);
         generatePath();
+        nextPath = null;
     }
 
     private void generatePath() {
-        Maze maze = Maze.getInstance();
         Position player = Player.getInstance().getPosition();
         path = getPath(getPosition(), player);
     }
 
-    private ArrayList<Position> getPathHelper(Position current, Position target, ArrayList<Position> path,
-            HashSet<Position> visited) {
+    // dfs solution
+    private ArrayList<Position> getPath(Position current, Position target) {
+        HashSet<Position> visited = new HashSet<Position>();
+        Queue<ArrayList<Position>> queue = new LinkedList<ArrayList<Position>>();
+        ArrayList<Position> initialPath = new ArrayList<Position>();
+        initialPath.add(current);
+        queue.add(initialPath);
         Maze maze = Maze.getInstance();
-        Cell cell = maze.getCell(current);
-        Position position = cell.getPosition();
 
-        // we've already looked here
-        if (visited.contains(position))
-            return null;
+        while (!queue.isEmpty()) {
+            ArrayList<Position> path = queue.remove();
 
-        visited.add(position);
+            for (Position position : path) {
+                visited.add(position);
+            }
 
-        // if cell is a wall, we can't move through it
-        if (cell.isWall())
-            return null;
+            Position last = path.get(path.size() - 1);
 
-        // if we have reached our target, there must be a path.
-        if (cell.getPosition().equals(target))
-            return path;
+            // if cell is a wall or another enemy
+            if (maze.getCell(last).isWall())
+                continue;
 
-        // if cell is not wall or path, get adjacent cells
-        ArrayList<Cell> adjacentCells = maze.getAdjacentCells(current);
-
-        // look at all adjacent cells and recurse
-        for (Cell adjacentCell : adjacentCells) {
-            Position adjacentPosition = adjacentCell.getPosition();
-
-            if (getPathHelper(adjacentPosition, target, path, visited) != null) {
-                path.add(adjacentPosition);
+            // if we have reached our target, this must be the shortest path
+            if (last.equals(target))
                 return path;
+
+            ArrayList<Position> adjacentPos = maze.getAdjacentPositions(last);
+
+            for (Position adjacent : adjacentPos) {
+                if (!visited.contains(adjacent)) {
+                    ArrayList<Position> newPath = new ArrayList<Position>(path);
+                    newPath.add(adjacent);
+                    queue.add(newPath);
+                }
             }
         }
 
         return null;
     }
 
-    private ArrayList<Position> getPath(Position current, Position target) {
-        ArrayList<Position> path = getPathHelper(current, target, new ArrayList<Position>(), new HashSet<Position>());
-        // simplify the path, remove any times it loops back on itself
-        HashMap<Position, Integer> map = new HashMap<>();
+    private void regeneratePath() {
+        Thread regenThread = new Thread(new Runnable() {
+            Position player = Player.getInstance().getPosition();
 
-        // basic O(n) solution to finding duplicates, add to hashmap
-        // add number of instances we've seen of this position in the hashmap as well
-        // if we've seen it before, increment the value
-        for (int i = 0; i < path.size(); i++) {
-            if (map.containsKey(path.get(i))) {
-                map.put(path.get(i), map.get(path.get(i)) + 1);
-            } else {
-                map.put(path.get(i), 1);
-            }
-        }
-
-        // filter out unnecessary moves, remove any times it loops back on itself
-        Position previous = null;
-        for (int i = 0; i < path.size(); i++) {
-            // get the number of instances we've seen of this position
-            previous = path.get(i);
-            int count = map.get(path.get(i));
-
-            while (count > 1 && i < path.size()) {
-                // update the hashmap with stuff we've see
-                Position pos = path.get(i);
-
-                if (pos == previous) {
-                    count--;
+            @Override
+            public void run() {
+                while (nextPath == null) {
+                    nextPath = getPath(getPosition(), player);
                 }
 
-                map.put(pos, map.get(pos) - 1);
-                // remove all paths that are after previous
-                path.remove(i);
-                i++;
-            }
-        }
+                System.out.println("regenerating path");
 
-        return path;
+                path = nextPath;
+                nextPath = null;
+            }
+        });
+
+        regenThread.start();
     }
 
     /***
@@ -104,19 +88,23 @@ public class Enemy extends CharacterModel {
      *
      */
     public void move() {
+        if (path == null) {
+            return;
+        }
+
+        // // pop last position from path and move to it
+        Position nextPos = path.remove(0);
+        setPosition(nextPos);
+
         // check if player has moved
         Player player = Player.getInstance();
 
-        // since path is generated from player to enemy, just look at the first position
+        // since path is generated from player to enemy, just look at the last position
         // in the path
-        Position lastPlayerPos = path.get(0);
+        Position lastPlayerPos = path.get(path.size() - 1);
 
         if (!player.getPosition().equals(lastPlayerPos)) {
-            generatePath();
+            regeneratePath();
         }
-
-        // pop last position from path and move to it
-        setPosition(path.get(path.size() - 1));
-        path.remove(path.size() - 1);
     }
 }
