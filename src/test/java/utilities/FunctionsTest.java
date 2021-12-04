f84ea37f4f49f9edd9b9f8438a20055ba039fe45
dylany <dylan_young@sfu.ca>
2021-12-04 06:40:09 +0000

    test: Random Position and Validating Position Tests

--- src/test/java/utilities/FunctionsTest.java
+++ src/test/java/utilities/FunctionsTest.java
@@ -39,4 +39,34 @@ class FunctionsTest {
             assertFalse(num2 < 0 || num2 > 10);
         }
     }
+
+    @Test
+    void getRandomPosition() {
+        Position pos = Functions.getRandomPosition();
+        assertNotNull(pos);
+    }
+
+    @Test
+    void getRandomPositionRange() {
+        int minX = 0;
+        int maxX = 10;
+        int minY = 0;
+        int maxY = 10;
+        for(int i = 0; i < 1000; i++){
+            Position pos = Functions.getRandomPosition(minX, maxX, minY, maxY);
+            assertNotNull(pos);
+            assertTrue(pos.getX() >= minX && pos.getX() <= maxX);
+            assertTrue(pos.getY() >= minY && pos.getY() <= maxY);
+            assertFalse(pos.getX() < minX && pos.getX() > maxX);
+            assertFalse(pos.getY() < minY && pos.getY() > maxY);
+        }
+    }
+
+    @Test
+    void validatePosition() {
+        Position validPos = new Position(1,1);
+        Position invalidPos = new Position(-1, -1);
+        assertTrue(Functions.validatePosition(validPos));
+        assertFalse(Functions.validatePosition(invalidPos));
+    }
 }
